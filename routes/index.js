1e2c4390a74f4e8642ae61a5a8d26782b56b1953
Zoe Sullivan <0442338+zothsu@users.noreply.github.com.>
2024-01-12 15:02:09 -0800

    add login functionality

--- routes/index.js
+++ routes/index.js
@@ -4,7 +4,7 @@ const passport = require('passport');
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('home', { title: 'The NeuroSpicy Camper' });
 });
 
 // Google OAuth login route
