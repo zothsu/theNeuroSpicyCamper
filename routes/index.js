fe3af222c4272b91287e2902ed0ea2836dbfcf66
Zoe Sullivan <0442338+zothsu@users.noreply.github.com.>
2024-01-12 13:54:57 -0800

    Define Routes for Authentication

--- routes/index.js
+++ routes/index.js
@@ -7,4 +7,25 @@ router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
 
+// Google OAuth login route
+router.get('/auth/google', passport.authenticate(
+  // Which passport strategy is being used?
+  'google',
+  {
+    // Requesting the user's profile and email
+    scope: ['profile', 'email'],
+    // Optionally force pick account every time
+    // prompt: "select_account"
+  }
+));
+
+// Google OAuth callback route
+router.get('/oauth2callback', passport.authenticate(
+  'google',
+  {
+    successRedirect: '/movies',
+    failureRedirect: '/movies'
+  }
+));
+
 module.exports = router;
