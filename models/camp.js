d874b5cacd407ef65725ff90e7fb7f516b461803
Zoe Sullivan <0442338+zothsu@users.noreply.github.com.>
2024-01-15 12:12:08 -0800

    mv review schema to fix bug

--- models/camp.js
+++ models/camp.js
@@ -1,6 +1,31 @@
 const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
+const reviewSchema = new Schema({
+  content: {
+    type: String,
+    required: true
+  },
+  rating: {
+    type: Number,
+    min: 1,
+    max: 5,
+    default: 5
+  },
+  // Don't forget to add the comma above then
+  // add the 3 new properties below
+  user: {
+    type: Schema.Types.ObjectId,
+    ref: 'User',
+    required: true
+  },
+  userName: String,
+  userAvatar: String
+}, {
+  timestamps: true
+});
+
+
 const campSchema = new Schema({
   name: { 
     type: String, 
@@ -28,29 +53,6 @@ const campSchema = new Schema({
 });
 
 
-const reviewSchema = new Schema({
-  content: {
-    type: String,
-    required: true
-  },
-  rating: {
-    type: Number,
-    min: 1,
-    max: 5,
-    default: 5
-  },
-  // Don't forget to add the comma above then
-  // add the 3 new properties below
-  user: {
-    type: Schema.Types.ObjectId,
-    ref: 'User',
-    required: true
-  },
-  userName: String,
-  userAvatar: String
-}, {
-  timestamps: true
-});
 
 
 module.exports = mongoose.model('Camp', campSchema);
