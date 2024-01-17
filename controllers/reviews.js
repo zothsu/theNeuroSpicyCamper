e3215a580208a74e7461f918fc35a0bf9f81e76f
Zoe Sullivan <0442338+zothsu@users.noreply.github.com.>
2024-01-17 11:12:34 -0800

    add review controller and begin create funtionality

---
+++ controllers/reviews.js
@@ -0,0 +1,25 @@
+const Camp = require('../models/camp');
+const camps = require('./camps');
+
+module.exports = {
+  create,
+}
+
+async function create(req, res) {
+  const camp = await Camp.findById(req.params.id);
+
+  // Add the user-centric info to req.body (the new review)
+  req.body.user = req.user._id;
+  req.body.userName = req.user.name;
+  req.body.userAvatar = req.user.avatar;
+
+  // We can push (or unshift) subdocs into Mongoose arrays
+  camp.reviews.push(req.body);
+  try {
+    // Save any changes made to the camp doc
+    await camp.save();
+  } catch (err) {
+    console.log(err);
+  }
+  res.redirect(`/camps/${camps._id}`);
+}
